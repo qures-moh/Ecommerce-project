9a3cdfbb1c9d01fb36606f55f351e302392cf415
Delta <qureshimohsin958@gmail.com>
2026-09-22 18:28:31 +0530

    Use environment variable for API URL

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "./store";
 
 const api=axios.create({
-      baseURL: "http://localhost:3000/api/",
+       baseURL: import.meta.env.VITE_API_URL,
       withCredentials:true
 });
 api.interceptors.request.use(
